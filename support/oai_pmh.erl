f4b5d92a6d8e81f0aadf827cd0a82b61e5323630
Michel Rijnders <michel@driebit.nl>
2018-07-13 10:55:00 +0200

    Delete trailing whitespace

--- support/oai_pmh.erl
+++ support/oai_pmh.erl
@@ -30,7 +30,7 @@ import([], Endpoint, UrlParams, Context) ->
 import([], _Endpoint, _UrlParams, undefined, _Context) ->
     %% Empty resumption token, so reached end of data
     ok;
-    
+
 import([], Endpoint, UrlParams, ResumptionToken, Context) ->
     {Records, NewResumptionToken} = list_records(Endpoint, UrlParams, ResumptionToken),
     import(Records, Endpoint, UrlParams, NewResumptionToken, Context);
@@ -43,15 +43,15 @@ import(Records, Endpoint, UrlParams, ResumptionToken, Context) ->
 list_records(Endpoint, UrlParams) ->
     Response = request(Endpoint, [{verb, "ListRecords"}] ++ UrlParams),
     {XmlRoot, _} = xmerl_scan:string(Response, [{space, normalize}]),
-    
+
     %% Retrieve resumption token
     ResumptionToken = ginger_xml:get_value("//resumptionToken", XmlRoot),
     {parse_records(XmlRoot), ResumptionToken}.
 
 %% @doc Execute resume request
 list_records(Endpoint, UrlParams, ResumptionToken) ->
-    %% Remove params that OAI-PMH does not allow in resume requests  
-    ResumeParams = proplists:delete(metadataPrefix, 
+    %% Remove params that OAI-PMH does not allow in resume requests
+    ResumeParams = proplists:delete(metadataPrefix,
         proplists:delete(set, UrlParams)
     ) ++ [{resumptionToken, ResumptionToken}],
     list_records(Endpoint, ResumeParams).
@@ -69,6 +69,6 @@ request(Endpoint, UrlParams) ->
         Response ->
             Response
     end.
-    
+
 parse_records(XmlRoot) ->
     xmerl_xpath:string("//record", XmlRoot).
